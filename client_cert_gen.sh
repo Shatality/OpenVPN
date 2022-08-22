131594b7db562216465a1326521280322cc95d6b
Vadim Shatalov <djshatalov@gmail.com>
2022-08-22 13:05:12 +0300

    full ovpn file done

--- client_cert_gen.sh
+++ client_cert_gen.sh
@@ -1,23 +1,28 @@
 #!/bin/bash
 # Запрашиваем имя нового клиента
-sudo read -p "Введите название папки для нового пользователя:" NewUser
+read -p "Введите название папки для нового пользователя:" NewUser
 # Создаем для него папку
-sudo mkdir /etc/openvpn/clients/$NewUser
+mkdir /etc/openvpn/clients/$NewUser
 # Генерируем сертификат пользователя
-sudo cd /etc/openvpn/easy-rsa/
-sudo ./easyrsa build-client-full $NewUser nopass
+cd /etc/openvpn/easy-rsa/
+./easyrsa build-client-full $NewUser nopass
 # Копируем сертификаты в папку пользователя
-sudo cp /etc/openvpn/easy-rsa/pki/ca.crt /etc/openvpn/clients/$NewUser
-sudo cp /etc/openvpn/easy-rsa/pki/ta.key /etc/openvpn/clients/$NewUser
-sudo cp /etc/openvpn/easy-rsa/pki/issued/$NewUser.crt /etc/openvpn/clients/$NewUser
-sudo cp /etc/openvpn/easy-rsa/pki/private/$NewUser.key /etc/openvpn/clients/$NewUser
+cp /etc/openvpn/easy-rsa/pki/ca.crt /etc/openvpn/clients/$NewUser
+cp /etc/openvpn/easy-rsa/pki/ta.key /etc/openvpn/clients/$NewUser
+cp /etc/openvpn/easy-rsa/pki/issued/$NewUser.crt /etc/openvpn/clients/$NewUser
+cp /etc/openvpn/easy-rsa/pki/private/$NewUser.key /etc/openvpn/clients/$NewUser
 # Переходим в папку для дальнейшей генерации в ней ключа
-sudo cd /etc/openvpn/clients/$NewUser
+cd /etc/openvpn/clients/$NewUser
 # Узнаем наш внешний ip адрес и вводим его
 ip -br a
-sudo read -p "Введите внешний ip адрес:" YourIP
+read -p "Введите внешний ip адрес:" YourIP
+# Присваиваем значения переменным, они потом добавятся в конфигурационный файл клиента
+UsrCert="$(head -84 $NewUser.crt | tail +65)"
+CatCA="$(cat ca.crt)"
+CatUsrKey="$(cat $NewUser.key)"
+CatTA="$(cat ta.key)"
 # Создаем конфигурационный файл пользователя
-sudo cat > /etc/openvpn/clients/$NewUser/$NewUser.conf <<EOF
+cat > /etc/openvpn/clients/$NewUser/$NewUser.ovpn <<EOF
 client
 dev tun
 proto udp
@@ -26,13 +31,22 @@ resolv-retry infinite
 nobind
 persist-key
 persist-tun
-ca ca.crt
-cert $NewUser.crt
-key $NewUser.key
 remote-cert-tls server
-tls-auth ta.key 1
 cipher AES-256-GCM
 auth-nocache
 verb 3
+<ca>
+$CatCA
+</ca>
+<cert>
+$UsrCert
+</cert>
+<key>
+$CatUsrKey
+</key>
+key-direction 1
+<tls-auth>
+$CatTA
+</tls-auth>
 EOF
 echo "Файлы клиентского сертификата находятся здесь /etc/openvpn/clients/$NewUser"
